f498dbcabc3beff69e716e7304ff80da37d323ea
emirontop1 <dincemr@gmail.com>
2026-06-29 00:10:25 +0300

    Add app/src/main/java/com/customkeyboard/settings/PrefsKeys.kt

---
+++ app/src/main/java/com/customkeyboard/settings/PrefsKeys.kt
@@ -0,0 +1,12 @@
+package com.customkeyboard.settings
+
+object PrefsKeys {
+    const val PREFS_NAME = "kb_prefs"
+    const val DARK_THEME = "dark_theme"
+    const val ACCENT_COLOR = "accent_color"
+    const val HAPTIC = "haptic_enabled"
+    const val SOUND = "sound_enabled"
+    const val AUTO_CONVERT = "auto_convert"
+    const val BANNED_WORDS = "banned_words"
+    const val LEARNED_WORDS = "learned_words"
+}
